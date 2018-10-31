51ad34f5d959e2ab2f9c7b32e57e406c146a4e1d
AmaroPSI <2170400@my.ipleiria.pt>
2018-10-31 11:18:25 +0000

    Added 1 element to Class Treino

--- app/src/main/java/com/example/melic/gymplan/classes/Treino.java
+++ app/src/main/java/com/example/melic/gymplan/classes/Treino.java
@@ -4,25 +4,25 @@ public class Treino {
 
     private int id;
     private int dificuldade;
+    private int repeticoes;
 
-    public Treino(int id, int dificuldade){
+
+    public Treino(int id, int dificuldade, int repeticoes){
         this.id = id;
         this.dificuldade=dificuldade;
+        this.repeticoes=repeticoes;
     }
 
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
 
-    public int getDificuldade() {
-        return dificuldade;
-    }
+    public int getDificuldade() { return dificuldade; }
+    public void setDificuldade(int Dificuldade) { this.dificuldade = Dificuldade; }
 
-    public void setDificuldade(int Dificuldade) {
-        this.dificuldade = Dificuldade;
-    }
+    public int getRepeticoes() { return repeticoes; }
+    public void setRepeticoes(int repeticoes) { this.repeticoes = repeticoes; }
 }
