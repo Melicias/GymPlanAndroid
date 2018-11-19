c292b784b7d853cf154d17a104617992b3364c63
Gonçalo Amaro <33254654+AmaroPSI@users.noreply.github.com>
2018-11-19 14:56:00 +0000

    Removed the e-mail thing from TreinoActivity

--- app/src/main/java/com/example/melic/gymplan/TreinoActivity.java
+++ app/src/main/java/com/example/melic/gymplan/TreinoActivity.java
@@ -1,7 +1,6 @@
 package com.example.melic.gymplan;
 
 import android.app.ActionBar;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -57,14 +56,6 @@ public class TreinoActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
 
     }
